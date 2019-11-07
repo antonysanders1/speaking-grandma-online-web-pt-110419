06169d58cc36e68f1e4c1239b595c4267e7f6736
Antony Sanders <antony.devon92@gmail.com>
2019-11-07 16:11:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
-speak_to_grandma = "I LOVE YOU GRANDMA!"
 
-if speak_to_grandma == "Hi!"
+def speak_to_grandma("I LOVE YOU GRANDMA!")
+  if speak_to_grandma == "Hi!"
     puts "HUH?! SPEAK UP, SONNY!"
   elsif speak_to_grandma == "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
@@ -10,6 +10,9 @@ if speak_to_grandma == "Hi!"
   elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
+end
+
+
   
   
   
