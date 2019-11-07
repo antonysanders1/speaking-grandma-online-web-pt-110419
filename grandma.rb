e3d18f2a424cad14e11b0b567abef287e6d09f74
Antony Sanders <antony.devon92@gmail.com>
2019-11-07 16:04:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,10 +3,10 @@ if speak_to_grandma == "Hi!"
     puts "HUH?! SPEAK UP, SONNY!"
   elsif speak_to_grandma == "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
-    elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
+  elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
-    elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
-    puts "HUH?! SPEAK UP, SONNY!"
+  elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
